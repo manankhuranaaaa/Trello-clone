c7ca8199dceab9b98e0437ac8b91c054bd1ac975
Manan Khurana <manankhurana@Manans-MacBook-Air.local>
2026-04-14 21:03:55 +0530

    fixed api base url for production

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: 'https://trello-clone-rrls.onrender.com/api'
+});
 
 // Boards
 export const getBoards = () => api.get('/boards').then(r => r.data);
